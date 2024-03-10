70f6e0e9cf1d2152db89d8a764aeb02b86c77b03
Oliver Brandt <oliver.brandt@hamburg.de>
2024-03-10 20:13:18 +0100

    feat: Implement command line call to train network.

--- train.py
+++ train.py
@@ -1,9 +1,17 @@
 from modules.cmdline_args import get_train_input_args
+from modules.flower_classifier import FlowerClassifier
 
 if __name__ == "__main__":  
     try:
         args = get_train_input_args()
-        print(args)
+        #print(args)
+
+        classifier = FlowerClassifier.from_training_data(
+            args.data_dir, args.save_dir, args.arch, args.learning_rate, args.hidden_units,
+            args.dropout_rate, args.epochs, args.gpu)
+        
+        classifier.train_model()
+        
     except ValueError as ve:
         print(f'Value error: {str(ve)}')
     except FileNotFoundError as fnfe:
